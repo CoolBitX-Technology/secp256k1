a5abb72bb9009fbe2a693092cf63c5387caf9000
Quincy Chang <skchang710@gmail.com>
2020-06-04 18:39:25 +0800

    support ECDH for secp256k1.swift

--- src/modules/ecdh/main_impl.h
+++ src/modules/ecdh/main_impl.h
@@ -8,16 +8,15 @@
 #define SECP256K1_MODULE_ECDH_MAIN_H
 
 #include "secp256k1_ecdh.h"
-#include "ecmult_const_impl.h"
+#include "../../ecmult_const_impl.h"
 
-int secp256k1_ecdh(const secp256k1_context* ctx, unsigned char *result, const secp256k1_pubkey *point, const unsigned char *scalar) {
+int secp256k1_ecdh(const secp256k1_context* ctx, const secp256k1_pubkey *point, const unsigned char *scalar) {
     int ret = 0;
     int overflow = 0;
     secp256k1_gej res;
     secp256k1_ge pt;
     secp256k1_scalar s;
     VERIFY_CHECK(ctx != NULL);
-    ARG_CHECK(result != NULL);
     ARG_CHECK(point != NULL);
     ARG_CHECK(scalar != NULL);
 
@@ -32,18 +31,7 @@ int secp256k1_ecdh(const secp256k1_context* ctx, unsigned char *result, const se
 
         secp256k1_ecmult_const(&res, &pt, &s, 256);
         secp256k1_ge_set_gej(&pt, &res);
-        /* Compute a hash of the point in compressed form
-         * Note we cannot use secp256k1_eckey_pubkey_serialize here since it does not
-         * expect its output to be secret and has a timing sidechannel. */
-        secp256k1_fe_normalize(&pt.x);
-        secp256k1_fe_normalize(&pt.y);
-        secp256k1_fe_get_b32(x, &pt.x);
-        y[0] = 0x02 | secp256k1_fe_is_odd(&pt.y);
-
-        secp256k1_sha256_initialize(&sha);
-        secp256k1_sha256_write(&sha, y, sizeof(y));
-        secp256k1_sha256_write(&sha, x, sizeof(x));
-        secp256k1_sha256_finalize(&sha, result);
+				secp256k1_pubkey_save(point, &pt);
         ret = 1;
     }
 
